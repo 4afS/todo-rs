c62fb9843d94ecfde1da5aa02117db535abe0454
4afS <4afS.h1@gmail.com>
2020-12-20 04:18:30 +0900

    fix typo

--- src/domain/repository.rs
+++ src/domain/repository.rs
@@ -1,7 +1,7 @@
 use crate::domain::todo::Todo;
 use crate::domain::id::Id;
 
-pub trait Repisitory<E> {
+pub trait Repository<E> {
     fn add(&self, todo: &Todo) -> Result<(), E>;
     fn delete(&self, id: &Id) -> Result<(), E>;
     fn get(&self, id: &Id) -> Result<Todo, E>;
