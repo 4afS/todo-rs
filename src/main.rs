65223ccd5c2cfbad5a7b08a33368f7d88b8542f2
4afS <4afS.h1@gmail.com>
2020-12-21 05:10:03 +0900

    add id menber

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use chrono::Local;
-use todo::domain::{due_to::DueTo, title::Title, todo::Todo};
+use todo::domain::{id, due_to::DueTo, title::Title, todo::Todo};
 
 fn main() {
     let due_to = DueTo::new(&Local::today().naive_local());
     let title = Title::new("title1");
-    let todo = Todo::new(&title, &due_to);
+    let id = id::generate();
+    let todo = Todo::new(&id, &title, &due_to);
     print!("{:?}", todo);
 }
