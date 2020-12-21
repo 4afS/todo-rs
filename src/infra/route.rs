321155226cfac52e9b362aa8abe718db2bc91c26
4afS <4afS.h1@gmail.com>
2020-12-22 00:13:12 +0900

    implement routing

--- src/infra/route.rs
+++ src/infra/route.rs
@@ -1,21 +1,33 @@
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
+use mock::Db;
 
-use crate::{interface::controller, usecase::todo::Usecase};
+use crate::{interface::todo, usecase::todo::Usecase};
 
 use super::db::mock;
 
-pub fn route(_: &mut web::ServiceConfig) {}
+extern crate env_logger;
 
-pub async fn run() -> Result<(), std::io::Error> {
-    let repository = mock::Db;
+pub fn route(config: &mut web::ServiceConfig) {
+    let repository = Db;
     let usecase = Usecase { repository };
-    let _ = controller::todo::Controller { usecase };
+
+    config.data(usecase);
+    config.route("/todo", web::post().to(todo::create::<Db>));
+    config.route("/todo", web::get().to(todo::all::<Db>));
+    config.route("/todo/{id}", web::delete().to(todo::delete::<Db>));
+    config.route("/todo/{id}", web::get().to(todo::get::<Db>));
+}
+
+#[actix_web::main]
+pub async fn run() -> Result<(), std::io::Error> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
 
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
-            .service(web::scope("/todo").configure(route))
+            .service(web::scope("/").configure(route))
     })
     .bind("0.0.0.0:8088")?
     .run()
