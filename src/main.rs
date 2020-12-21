768444b1027360f4280da0a338bba65cb4cc272c
4afS <4afS.h1@gmail.com>
2020-12-22 00:13:27 +0900

    run main function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
-use chrono::Local;
-use todo::{domain::{due_to::DueTo, id, repository::Repository, title::Title, todo::Todo}, interface::controller::todo::Controller, usecase::todo::Usecase};
+use todo::infra::route::run;
 
 fn main() {
-    let due_to = DueTo::new(&Local::today().naive_local());
-    let title = Title::new("title1");
-    let id = id::generate();
-    let todo = Todo::new(&id, &title, &due_to);
-
-    print!("{:?}", todo);
+    let _ = run();
 }
